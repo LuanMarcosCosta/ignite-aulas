daaef0fd7ba0cc4b41261abd2f31504c45442b03
LuanMarcosCosta <luanmarcoscosta@outlook.com>
2021-09-12 18:20:10 -0300

    Firs Commit EaD2

---
+++ reactjs/dtmoney/src/App.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+     <GlobalStyle/>
+    </div>
+  );
+}
+
