939cf8dbd34035d227351381f06e3d6847bc55f0
LuanMarcosCosta <luanmarcoscosta@outlook.com>
2021-09-13 06:50:21 -0300

    Updates Initials

---
+++ reactjs/dtmoney/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+export function Header() {
+    return (
+        <Container>
+            <Content>
+                <img src="" alt="DT Money" />
+                <button type="button">
+                    Nova Transação
+                </button>
+            </Content>
+        </Container>
+    )
+}
